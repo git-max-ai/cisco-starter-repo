ffb2dc027350d067e2824b1e0fde866dfaf816e2
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2022-07-24 05:15:09 +0530

    add

---
+++ sextant/src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="Banner">
+      <h1>SEXTANT</h1>
+    </div>
+  )
+}
+
+export default Banner
