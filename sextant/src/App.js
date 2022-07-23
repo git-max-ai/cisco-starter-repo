25e840a26bbd05fb635496486bea1042a01862f0
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2022-07-24 05:27:09 +0530

    update

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Component } from 'react';
 import Title from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Exhibit name="I'm an exhibit!"></Exhibit>
+                <Exhibit name="I'm also an exhibit!"></Exhibit>
+                <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
     </div>
   );
 }
