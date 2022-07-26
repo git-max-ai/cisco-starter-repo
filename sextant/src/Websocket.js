92e2a392d3dd1cab2f08d80aeaad6f6f25a15a73
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2022-07-27 04:22:53 +0530

    add

---
+++ sextant/src/Websocket.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://localhost:55455');
+
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        Practical Intro To WebSockets.
+      </div>
+    );
+  }
+}
+
+export default App;
